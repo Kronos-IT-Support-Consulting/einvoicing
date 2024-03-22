ee17ae7c97f1f9bb6435353986c80695c500c35b
Robert Stanciu <stanciu.robert.gabriel@gmail.com>
2024-03-22 11:56:18 +0200

    fix rounding

--- src/InvoiceLine.php
+++ src/InvoiceLine.php
@@ -289,7 +289,7 @@ class InvoiceLine {
      * @return self                Invoice line instance
      */
     public function setBaseQuantity(float $baseQuantity): self {
-        $this->baseQuantity = $baseQuantity != 0 ? $baseQuantity : 1.0;
+        $this->baseQuantity = $baseQuantity != 0.0 ? $baseQuantity : 1.0;
         return $this;
     }
 
