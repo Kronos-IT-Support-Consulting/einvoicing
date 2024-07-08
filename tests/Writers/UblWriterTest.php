cc88f935c0b7e9f1bb09907b03ccfbbd82fb9cfe
Robert Stanciu <stanciu.robert.gabriel@gmail.com>
2024-07-08 15:41:31 +0300

    feat: add project reference

--- tests/Writers/UblWriterTest.php
+++ tests/Writers/UblWriterTest.php
@@ -78,7 +78,6 @@ final class UblWriterTest extends TestCase {
             ->addPrecedingInvoiceReference(new InvoiceReference('INV-123'))
             ->setTenderOrLotReference('PPID-123')
             ->setContractReference('123Contractref')
-            ->setProjectReference('123projref')
             ->setSeller($seller)
             ->setBuyer($buyer)
             ->addLine($complexLine)
